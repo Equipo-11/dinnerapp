03a804dc49ff6c7cd35ca92f396ac72289c6d715
iremarmar <ire325@gmail.com>
2016-05-16 22:35:16 +0200

    no message

--- AppPrueba/app/src/main/java/com/example/luis/appprueba/ChangePassword.java
+++ AppPrueba/app/src/main/java/com/example/luis/appprueba/ChangePassword.java
@@ -2,6 +2,7 @@ package com.example.luis.appprueba;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class ChangePassword extends AppCompatActivity {
 
@@ -9,5 +10,11 @@ public class ChangePassword extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_change_password);
+    }
+    public void cancelButtonPass(View view){
+        finish();
+    }
+    public void changePassword (View view){
+
     }
 }
